7ab7ba6991fa33d6593171b0bfda3a85a810bbbe
dosssik <dosssik@ya.ru>
2020-06-08 21:16:57 +0200

    game over label, pause logic

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from hero import HeroState
 import random
 from enemy import Enemy
 from enemy import enemy_size
+from text import TextObject
 
 pygame.init()
 screen_width = 800
@@ -11,9 +12,10 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 screen_background_image = pygame.image.load("images/game_bg.jpg")
 
-pygame.time.set_timer(pygame.USEREVENT+1, 700)
+pygame.time.set_timer(pygame.USEREVENT + 1, 700)
 clock = pygame.time.Clock()
 game_finished = False
+game_paused = False
 
 moving_step = 10
 
@@ -24,23 +26,23 @@ hero_location_x = screen_width / 2
 hero_location_y = screen_height - hero_height
 
 isInJump = False
-jumpCounter = 20
+jump_counter = 20
 hero = Hero(hero_height, hero_width)
 
 enemies = []
 
 
 def do_jump():
-    global isInJump, jumpCounter, hero_location_y
-    if jumpCounter >= -20:
-        if jumpCounter < 0:
-            hero_location_y += (jumpCounter ** 2) / 9
+    global isInJump, jump_counter, hero_location_y
+    if jump_counter >= -20:
+        if jump_counter < 0:
+            hero_location_y += (jump_counter ** 2) / 9
         else:
-            hero_location_y -= (jumpCounter ** 2) / 9
-        jumpCounter -= 1
+            hero_location_y -= (jump_counter ** 2) / 9
+        jump_counter -= 1
     else:
         isInJump = False
-        jumpCounter = 20
+        jump_counter = 20
 
 
 def draw_window():
@@ -61,27 +63,21 @@ def createEnemy():
 
 
 def check_if_hero_is_dead():
-    global game_finished
+    global game_paused
     for enemy in enemies:
-        print(hero_location_x)
-        print(enemy.rect.centerx)
-        print(hero_location_x + hero_width)
-        print("")
         x_collision = hero_location_x <= enemy.rect.centerx <= (hero_location_x + hero_width)
         y_collision = hero_location_y + hero_height >= enemy.y
         if x_collision and y_collision:
-            game_finished = True
+            game_paused = True
 
 
-while not game_finished:
-    check_if_hero_is_dead()
-
+def main_game_flow():
+    global game_finished, isInJump, hero_location_x
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_finished = True
-        elif event.type == pygame.USEREVENT+1 and random.randint(0, 100) > 30:
+        elif event.type == pygame.USEREVENT + 1 and random.randint(0, 100) > 30:
             createEnemy()
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and hero_location_x > moving_step:
         hero.update(HeroState.left)
@@ -89,7 +85,6 @@ while not game_finished:
     elif keys[pygame.K_RIGHT] and hero_location_x < screen_width - hero_width - moving_step:
         hero.update(HeroState.right)
         hero_location_x += moving_step
-
     if isInJump:
         hero.update(HeroState.jump)
         do_jump()
@@ -97,6 +92,33 @@ while not game_finished:
         isInJump = True
     elif not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT] and not keys[pygame.K_SPACE]:
         hero.update(HeroState.idle)
-
     draw_window()
+
+
+def show_pause_state():
+    global game_paused, game_finished, enemies, hero_location_x, hero_location_y, jump_counter
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_finished = True
+        elif event.type == pygame.KEYDOWN:
+            enemies = []
+            pygame.event.clear(pygame.KEYDOWN)
+            pygame.event.clear(pygame.K_SPACE)
+            hero_location_x = screen_width / 2
+            hero_location_y = screen_height - hero_height
+            jump_counter = 20
+            game_paused = False
+
+    text = TextObject(screen_width / 2, screen_height / 2, lambda: f'GAME OVER! :( Press any key to try again')
+    text.draw(screen, True)
+    pygame.display.update()
+
+
+# MAIN LOOP
+while not game_finished:
+    if game_paused:
+        show_pause_state()
+    else:
+        check_if_hero_is_dead()
+        main_game_flow()
     clock.tick(80)
