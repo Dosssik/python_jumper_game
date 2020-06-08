a7b8bbe346159a596420e040a1f4e9fb3e92ed3d
dosssik <dosssik@ya.ru>
2020-06-08 10:39:59 +0200

    bounds and refactoring

--- game.py
+++ game.py
@@ -1,17 +1,19 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 game_finished = False
 
-speed = 10
+moving_step = 10
 
 x = 100
 y = 100
 
-height = 50
-width = 50
+rect_height = 50
+rect_width = 50
 
 while not game_finished:
     for event in pygame.event.get():
@@ -19,16 +21,17 @@ while not game_finished:
             game_finished = True
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    elif keys[pygame.K_RIGHT]:
-        x += speed
-    elif keys[pygame.K_DOWN]:
-        y += speed
-    elif keys[pygame.K_UP]:
-        y -= speed
+    if keys[pygame.K_LEFT] and x > moving_step:
+        x -= moving_step
+    elif keys[pygame.K_RIGHT] and x < screen_width - rect_width - moving_step:
+        x += moving_step
+    elif keys[pygame.K_DOWN] and y < screen_height - rect_height - moving_step:
+        y += moving_step
+    elif keys[pygame.K_UP] and y > moving_step:
+        y -= moving_step
 
     screen.fill((192, 192, 255))
-    pygame.draw.rect(screen, (100, 100, 100), (x, y, height, width))
+    rect_background = (100, 100, 100)
+    pygame.draw.rect(screen, rect_background, (x, y, rect_height, rect_width))
     pygame.display.update()
     clock.tick(60)
